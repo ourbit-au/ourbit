d07eb44e75ace9a6fc88e1e035eadbdcf65fd24b
Alex Thomas <Alex-thomas92@outlook.com>
2024-07-09 12:37:10 +0100

    Type fixes

--- app/api/scraper/route.ts
+++ app/api/scraper/route.ts
@@ -1,17 +1,38 @@
 import { createClient } from "@/utils/supabase/server";
 import { NextResponse } from "next/server";
-
 import puppeteer from "puppeteer";
 import { v4 as uuidv4 } from "uuid";
 
-const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+const sleep = (ms: number | undefined) =>
+  new Promise((resolve) => setTimeout(resolve, ms));
 
 export async function GET(request: Request) {
   console.log("GET request received");
   const supabase = createClient();
 
+  type ConfigOption = {
+    comparison: string;
+    where: string;
+    postcode: string;
+    energyConcession: string;
+    solarPanels: string;
+    peopleInHome: string;
+    roomsInHome: string;
+    fridges: string;
+    gasConnection: string;
+    heatingHome: string | string[];
+    coolingHome: string | string[];
+    dryer: string;
+    dryerUsage?: string;
+    hotWaterSystem: string;
+    controlledLoad: string;
+    seaDistance: string;
+    washingMachine: string;
+    washingMachineUsage: string;
+  };
+
   //default config to seed db
-  const options = [
+  const options: ConfigOption[] = [
     {
       comparison: "electricity",
       where: "home",
@@ -33,8 +54,10 @@ export async function GET(request: Request) {
     },
   ];
 
-  const insertConfiguration = async (config) => {
-    const mapConfigToDbColumns = (config) => {
+  const insertConfiguration = async (
+    config: ConfigOption & { created_at: string; updated_at: string }
+  ) => {
+    const mapConfigToDbColumns = (config: Record<string, any>) => {
       const mapping = {
         where: "location",
         controlledLoad: "controlledload",
@@ -51,7 +74,7 @@ export async function GET(request: Request) {
         washingMachineUsage: "washingmachineusage",
       };
 
-      return Object.keys(config).reduce((acc, key) => {
+      return Object.keys(config).reduce((acc: Record<string, any>, key) => {
         const dbColumn = mapping[key] || key.toLowerCase();
         acc[dbColumn] = config[key];
         return acc;
@@ -78,14 +101,14 @@ export async function GET(request: Request) {
     return data[0];
   };
 
-  async function getServerCacheId(config) {
+  async function getServerCacheId(config: ConfigOption) {
     const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
 
     await page.setViewport({ width: 1200, height: 800 });
 
-    let serverCacheId = null;
-    let offersData = null;
+    let serverCacheId: string | null = null;
+    let offersData: any = null;
 
     await page.setRequestInterception(true);
     page.on("request", (request) => {
@@ -119,7 +142,7 @@ export async function GET(request: Request) {
     });
 
     try {
-      async function forceClick(selector) {
+      async function forceClick(selector: string) {
         const elementHandle = await page.$(selector);
         if (!elementHandle) {
           console.error(`${selector} not found in the DOM`);
@@ -141,13 +164,17 @@ export async function GET(request: Request) {
         if (!isVisible) {
           await page.evaluate((selector) => {
             const element = document.querySelector(selector);
-            element.click();
+            if (element) {
+              (element as HTMLElement).click();
+            }
           }, selector);
         } else {
           await page.evaluate((selector) => {
             const element = document.querySelector(selector);
-            element.scrollIntoView();
-            element.click();
+            if (element) {
+              element.scrollIntoView();
+              (element as HTMLElement).click();
+            }
           }, selector);
         }
       }
@@ -269,99 +296,102 @@ export async function GET(request: Request) {
         offersData.offers.Electricity.offersList.length
       );
 
-      const offerEntries = [];
-      for (const offer of offersData.offers.Electricity.offersList) {
-        // const uniqueId = generateUniqueId(offer.offerDetails[0], configId);
-        const details = offer.offerDetails[0];
+      const offerEntries = offersData.offers.Electricity.offersList.map(
+        (offer: any) => {
+          // const uniqueId = generateUniqueId(offer.offerDetails[0], configId);
+          const details = offer.offerDetails[0];
 
-        const formatDate = (dateString: string | undefined): string | null => {
-          if (!dateString) return null;
+          const formatDate = (
+            dateString: string | undefined
+          ): string | null => {
+            if (!dateString) return null;
 
-          const [day, month, year] = dateString.split("/");
+            const [day, month, year] = dateString.split("/");
 
-          const dateObj = new Date(
-            parseInt(year),
-            parseInt(month) - 1,
-            parseInt(day)
-          );
+            const dateObj = new Date(
+              parseInt(year),
+              parseInt(month) - 1,
+              parseInt(day)
+            );
 
-          // Check if the date object is valid because sometimes we get an issue with the date format
-          if (isNaN(dateObj.getTime())) {
-            console.warn(`Invalid date format for input: ${dateString}`);
-            return null; // Return null for invalid dates
-          }
+            // Check if the date object is valid because sometimes we get an issue with the date format
+            if (isNaN(dateObj.getTime())) {
+              console.warn(`Invalid date format for input: ${dateString}`);
+              return null; // Return null for invalid dates
+            }
 
-          return dateObj.toISOString();
-        };
+            return dateObj.toISOString();
+          };
 
-        const offerEntry = {
-          //all of the fields from the offer object
-          id: uuidv4(),
-          offer_id: details.offerId,
-          config_id: configId,
-          price: details.basePrice,
-          retailer_name: details.retailerName,
-          offer_name: details.offerName,
-          tariff_type: details.tariffType,
-          distributor: details.distributor,
-          release_date: formatDate(details.releaseDate),
-          contract_length: details.contractLength,
-          green_power: details.greenPower,
-          solar: details.solar,
-          fees: details.fees,
-          discounts: details.discounts,
-          eligibility_criteria: details.eligibilityCriteria,
-          is_residential: details.isResidential,
-          retailer_license_name: details.retailerLicenseName,
-          retailer_fueltype: details.retailerFueltype,
-          retailer_url: details.retailerUrl,
-          retailer_image_url: details.retailerImageUrl,
-          retailer_phone: details.retailerPhone,
-          retailer_description: details.retailerDescription,
-          base_price: details.basePrice,
-          total_conditional: details.totalConditional,
-          total_unconditional: details.totalUnconditional,
-          total_inc_gst: details.totalIncGst,
-          total_exc_gst: details.totalExcGst,
-          includes_demand_tariff: details.includesDemandTariff,
-          annual_fee_count: details.annualFeeCount,
-          contract: details.contract,
-          solar_credit: details.solarCredit,
-          greenpower_amount: details.greenpowerAmount,
-          is_generally_available: details.isGenerallyAvailable,
-          greenpower_charge_type: details.greenpowerChargeType,
-          intrinsic_greenpower_percentage:
-            details.intrinsicGreenpowerPercentage,
-          parent_offer_has_greenpower_options:
-            details.parentOfferHasGreenpowerOptions,
-          is_control_load: details.isControlLoad,
-          is_tou_offer: details.isTouOffer,
-          base_offer_type: details.baseOfferType,
-          is_innovative_offer: details.isInnovativeOffer,
-          is_victorian_default_offer: details.isVictorianDefaultOffer,
-          is_direct_debit_only: details.isDirectDebitOnly,
-          has_pay_on_time_discount: details.hasPayOnTimeDiscount,
-          has_incentive: details.hasIncentive,
-          is_contingent_offer: details.isContingentOffer,
-          contract_length_count: details.contractLengthCount,
-          exit_fee: details.exitFee,
-          exit_fee_count: details.exitFeeCount,
-          is_gas_offer: details.isGasOffer,
-          is_sme: details.isSme,
-          estimated_solar_credit: details.estimatedSolarCredit,
-          incentives: details.incentives,
-          additional_fee_information: details.additionalFeeInformation,
-          solar_type: details.solarType,
-          cool_off_period: formatDate(details.coolOffPeriod),
-          is_available_for_solar: details.isAvailableForSolar,
-          is_custom_eligibility_offer: details.isCustomEligibilityOffer,
-          time_definition: details.timeDefinition,
-          created_at: new Date().toISOString(),
-          updated_at: new Date().toISOString(),
-        };
+          const offerEntry = {
+            //all of the fields from the offer object
+            id: uuidv4(),
+            offer_id: details.offerId,
+            config_id: configId,
+            price: details.basePrice,
+            retailer_name: details.retailerName,
+            offer_name: details.offerName,
+            tariff_type: details.tariffType,
+            distributor: details.distributor,
+            release_date: formatDate(details.releaseDate),
+            contract_length: details.contractLength,
+            green_power: details.greenPower,
+            solar: details.solar,
+            fees: details.fees,
+            discounts: details.discounts,
+            eligibility_criteria: details.eligibilityCriteria,
+            is_residential: details.isResidential,
+            retailer_license_name: details.retailerLicenseName,
+            retailer_fueltype: details.retailerFueltype,
+            retailer_url: details.retailerUrl,
+            retailer_image_url: details.retailerImageUrl,
+            retailer_phone: details.retailerPhone,
+            retailer_description: details.retailerDescription,
+            base_price: details.basePrice,
+            total_conditional: details.totalConditional,
+            total_unconditional: details.totalUnconditional,
+            total_inc_gst: details.totalIncGst,
+            total_exc_gst: details.totalExcGst,
+            includes_demand_tariff: details.includesDemandTariff,
+            annual_fee_count: details.annualFeeCount,
+            contract: details.contract,
+            solar_credit: details.solarCredit,
+            greenpower_amount: details.greenpowerAmount,
+            is_generally_available: details.isGenerallyAvailable,
+            greenpower_charge_type: details.greenpowerChargeType,
+            intrinsic_greenpower_percentage:
+              details.intrinsicGreenpowerPercentage,
+            parent_offer_has_greenpower_options:
+              details.parentOfferHasGreenpowerOptions,
+            is_control_load: details.isControlLoad,
+            is_tou_offer: details.isTouOffer,
+            base_offer_type: details.baseOfferType,
+            is_innovative_offer: details.isInnovativeOffer,
+            is_victorian_default_offer: details.isVictorianDefaultOffer,
+            is_direct_debit_only: details.isDirectDebitOnly,
+            has_pay_on_time_discount: details.hasPayOnTimeDiscount,
+            has_incentive: details.hasIncentive,
+            is_contingent_offer: details.isContingentOffer,
+            contract_length_count: details.contractLengthCount,
+            exit_fee: details.exitFee,
+            exit_fee_count: details.exitFeeCount,
+            is_gas_offer: details.isGasOffer,
+            is_sme: details.isSme,
+            estimated_solar_credit: details.estimatedSolarCredit,
+            incentives: details.incentives,
+            additional_fee_information: details.additionalFeeInformation,
+            solar_type: details.solarType,
+            cool_off_period: formatDate(details.coolOffPeriod),
+            is_available_for_solar: details.isAvailableForSolar,
+            is_custom_eligibility_offer: details.isCustomEligibilityOffer,
+            time_definition: details.timeDefinition,
+            created_at: new Date().toISOString(),
+            updated_at: new Date().toISOString(),
+          };
 
-        offerEntries.push(offerEntry);
-      }
+          offerEntries.push(offerEntry);
+        }
+      );
 
       //changed to bulk insert because of the number of offers
       const { data, error } = await supabase
